7dcaa5963bbfba0168ec61ef61d5f705f5cfca06
root <root@linux.home>
2022-05-31 15:45:25 +0200

    ajout de l'association ManyToOne dans Compte

--- src/main/java/com/inti/entities/Compte.java
+++ src/main/java/com/inti/entities/Compte.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Compte implements Serializable{
@@ -15,6 +17,9 @@ public class Compte implements Serializable{
 	private Long idCompte;
 	private Date dateCreation;
 	private double solde;
+	@ManyToOne
+	@JoinColumn(name="id_user")
+	private Utilisateur utilisateur;
 
 	public Compte() {
 
@@ -25,6 +30,14 @@ public class Compte implements Serializable{
 		this.solde = solde;
 	}
 
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
 
 	public Long getIdCompte() {
 		return idCompte;
